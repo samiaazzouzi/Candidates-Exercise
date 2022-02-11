1dd18ea72096415ca015c402c9c450cf1e940722
samiaazzouzi <68155401+samiaazzouzi@users.noreply.github.com>
2022-02-11 23:00:23 +0100

    initial server config

---
+++ Tipaw-backend/src/main/index.ts
@@ -0,0 +1,19 @@
+// comment app initialization when building and packaging the project
+// import 'reflect-metadata';
+
+// import { createExpressServer, useExpressServer } from 'routing-controllers';
+
+// import { PostgresService } from './services/PostgresService';
+// const fs = require('fs');
+
+// const url = JSON.parse(fs.readFileSync(process.env.AUTH_CONFIG_PATH, 'utf8')).mongoConfig.url ;
+
+// PostgresService.getInstance().connect(url);
+// const app = createExpressServer({
+// });
+
+// // run express application on port 3000
+// app.listen(3000, () => {
+//   console.log('Server started ');
+// });
+

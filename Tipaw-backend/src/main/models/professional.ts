e424e4b260b593994eb775cbc82655d35fd491eb
samiaazzouzi <68155401+samiaazzouzi@users.noreply.github.com>
2022-02-12 12:28:12 +0100

    init prisma

--- Tipaw-backend/src/main/models/professional.ts
+++ Tipaw-backend/src/main/models/professional.ts
@@ -1,7 +1,14 @@
 export interface Professional {
     firstname: string;
     lastname: string;
+    langue: string;
+    title: string;
     email: string;
-    password: string;
+    seo: string;
+    description: string;
+    address: string;
+    picture: string;
+    paiment_option: string;
+    appointment_schedule: string;
 
 }

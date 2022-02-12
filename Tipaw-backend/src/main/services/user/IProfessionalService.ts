ee93c4f289a87df165523903facc677580f8bfb2
samiaazzouzi <68155401+samiaazzouzi@users.noreply.github.com>
2022-02-12 17:15:13 +0100

    move frontend into Tipaw-frontend

--- Tipaw-backend/src/main/services/user/IProfessionalService.ts
+++
@@ -1,8 +0,0 @@
-import { Professional } from "../../models/professional";
-
-export interface IProfessionalService {
-    getAllProfessionals(): Professional[];
-    getProfessionalById(id: string): Promise<Professional>;
-    createProfessional(professional: Professional): Promise<Professional>;
-
-}
